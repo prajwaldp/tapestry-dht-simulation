bc76a440cacac559dd22dd3df9e62e7316e22120
Prajwal Prakash <prajwalpy6@gmail.com>
2019-10-25 13:39:21 -0400

    Add running instructions to the README

--- config/config.exs
+++ config/config.exs
@@ -1,6 +1,6 @@
 use Mix.Config
 
-config :logger, level: :info
+config :logger, level: :debug
 
 # Time delay after which a node sends the next request in ms
 config :tapestry, :delay_between_reqs, 1000
