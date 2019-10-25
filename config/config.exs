593e498b8329130c2f6c539327e68e7b875a91dc
Prajwal Prakash <prajwalpy6@gmail.com>
2019-10-25 15:02:09 -0400

    Set default debug config to :info

--- config/config.exs
+++ config/config.exs
@@ -1,6 +1,6 @@
 use Mix.Config
 
-config :logger, level: :debug
+config :logger, level: :info
 
 # Time delay after which a node sends the next request in ms
 config :tapestry, :delay_between_reqs, 1000
